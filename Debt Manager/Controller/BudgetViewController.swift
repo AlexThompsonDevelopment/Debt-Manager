41910355d29d7297e3764bd338a504d640ae1850
Alex <alexthompsondevelopment@gmail.com>
2021-04-09 16:21:26 +1000

    build out debt page tableview

--- Debt Manager/Controller/BudgetViewController.swift
+++ Debt Manager/Controller/BudgetViewController.swift
@@ -9,14 +9,13 @@ import UIKit
 
 class BudgetViewController: UIViewController {
 
-    
     @IBOutlet weak var tableView: UITableView!
-    var data = Data()
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         //use userdefaults to save total and load it here. 
-        tableView.reloadData()
+        
     }
     
     
@@ -25,42 +24,39 @@ class BudgetViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         registerCell()
-        
     }
+    
+    
 }
 
 extension BudgetViewController: UITableViewDelegate, UITableViewDataSource {
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
-        
     }
     
     private func registerCell() {
-        let cell = UINib(nibName: "TableViewCell", bundle: nil)
-        self.tableView.register(cell, forCellReuseIdentifier: "CustomCell")
-    }
+           let cell = UINib(nibName: "BudgetTableViewCell", bundle: nil)
+           self.tableView.register(cell, forCellReuseIdentifier: "CustomCell")
+       }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as? TableViewCell else {
-            return UITableViewCell()
-        }
-        let topics = ["Income", "Expenses", "Savings"]
-        let values = ["0", "0", "0"]
-        cell.topicLabel.text = topics[indexPath.row]
-        cell.valueLabel.text = values[indexPath.row]
-        return cell
-    }
+         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! BudgetTableViewCell
+         
+         let topics = ["Income", "Expenses", "Savings"]
+        cell.cellName.text = topics[indexPath.row]
+         return cell
+     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //implement tableview reusefunctionality here
-        performSegue(withIdentifier: "ExpenseSegue", sender: self)
-    }
-    
-    
-    
-    
-    
-    
+            performSegue(withIdentifier: "ExpenseSegue", sender: self)
+        }
+        
 }
 
+    
+    
+    
+    
+    
+    
+    
